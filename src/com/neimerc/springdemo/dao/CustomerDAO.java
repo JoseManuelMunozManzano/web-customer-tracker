c0d7708eaad825284defea48e28215c14ef238c6
Neimerc <Neil_Mercury@yahoo.es>
2019-10-14 21:22:07 +0200

    Defining DAO interface

---
+++ src/com/neimerc/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.neimerc.springdemo.dao;
+
+import java.util.List;
+
+import com.neimerc.sprindemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();	
+	
+}
