f420b7746a54c95a7529239067fd7a539f3bcdaa
Neimerc <Neil_Mercury@yahoo.es>
2019-10-16 21:19:04 +0200

    Added the new customer to the database

--- src/com/neimerc/springdemo/dao/CustomerDAOImpl.java
+++ src/com/neimerc/springdemo/dao/CustomerDAOImpl.java
@@ -35,4 +35,15 @@ public class CustomerDAOImpl implements CustomerDAO {
 		return customers;
 	}
 
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save the customer
+		currentSession.save(theCustomer);
+		
+	}
+
 }
