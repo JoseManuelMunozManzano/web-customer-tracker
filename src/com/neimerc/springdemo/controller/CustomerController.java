43740b373daf9a37aa5cffc6d46ff3ddc40cc349
Neimerc <Neil_Mercury@yahoo.es>
2019-10-16 21:02:44 +0200

    Added a new Add Customer button, with its new jsp

--- src/com/neimerc/springdemo/controller/CustomerController.java
+++ src/com/neimerc/springdemo/controller/CustomerController.java
@@ -31,4 +31,15 @@ public class CustomerController {
 		return "list-customers";
 	}
 	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Customer theCustomer = new Customer();
+		
+		theModel.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+	}
+	
 }
